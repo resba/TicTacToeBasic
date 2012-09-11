2072a5cee39f8337dd31ee53e66778fe0f8d90cc
Matt Sowden <resba@mng.(none)>
2012-09-12 03:49:20 +0400

    Initial Commit

---
+++ TicTacToeBasic/Form2.Designer.vb
@@ -0,0 +1,164 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form2
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.C1R1 = New System.Windows.Forms.Button()
+        Me.C2R1 = New System.Windows.Forms.Button()
+        Me.C3R1 = New System.Windows.Forms.Button()
+        Me.C3R2 = New System.Windows.Forms.Button()
+        Me.C2R2 = New System.Windows.Forms.Button()
+        Me.C1R2 = New System.Windows.Forms.Button()
+        Me.C3R3 = New System.Windows.Forms.Button()
+        Me.C2R3 = New System.Windows.Forms.Button()
+        Me.C1R3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'C1R1
+        '
+        Me.C1R1.BackColor = System.Drawing.SystemColors.Desktop
+        Me.C1R1.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.C1R1.Location = New System.Drawing.Point(13, 13)
+        Me.C1R1.Name = "C1R1"
+        Me.C1R1.Size = New System.Drawing.Size(222, 198)
+        Me.C1R1.TabIndex = 0
+        Me.C1R1.Text = "-"
+        Me.C1R1.UseVisualStyleBackColor = False
+        '
+        'C2R1
+        '
+        Me.C2R1.BackColor = System.Drawing.SystemColors.Desktop
+        Me.C2R1.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.C2R1.Location = New System.Drawing.Point(241, 13)
+        Me.C2R1.Name = "C2R1"
+        Me.C2R1.Size = New System.Drawing.Size(222, 198)
+        Me.C2R1.TabIndex = 1
+        Me.C2R1.Text = "-"
+        Me.C2R1.UseVisualStyleBackColor = False
+        '
+        'C3R1
+        '
+        Me.C3R1.BackColor = System.Drawing.SystemColors.Desktop
+        Me.C3R1.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.C3R1.Location = New System.Drawing.Point(469, 13)
+        Me.C3R1.Name = "C3R1"
+        Me.C3R1.Size = New System.Drawing.Size(222, 198)
+        Me.C3R1.TabIndex = 2
+        Me.C3R1.Text = "-"
+        Me.C3R1.UseVisualStyleBackColor = False
+        '
+        'C3R2
+        '
+        Me.C3R2.BackColor = System.Drawing.SystemColors.Desktop
+        Me.C3R2.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.C3R2.Location = New System.Drawing.Point(469, 217)
+        Me.C3R2.Name = "C3R2"
+        Me.C3R2.Size = New System.Drawing.Size(222, 198)
+        Me.C3R2.TabIndex = 5
+        Me.C3R2.Text = "-"
+        Me.C3R2.UseVisualStyleBackColor = False
+        '
+        'C2R2
+        '
+        Me.C2R2.BackColor = System.Drawing.SystemColors.Desktop
+        Me.C2R2.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.C2R2.Location = New System.Drawing.Point(241, 217)
+        Me.C2R2.Name = "C2R2"
+        Me.C2R2.Size = New System.Drawing.Size(222, 198)
+        Me.C2R2.TabIndex = 4
+        Me.C2R2.Text = "-"
+        Me.C2R2.UseVisualStyleBackColor = False
+        '
+        'C1R2
+        '
+        Me.C1R2.BackColor = System.Drawing.SystemColors.Desktop
+        Me.C1R2.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.C1R2.Location = New System.Drawing.Point(13, 217)
+        Me.C1R2.Name = "C1R2"
+        Me.C1R2.Size = New System.Drawing.Size(222, 198)
+        Me.C1R2.TabIndex = 3
+        Me.C1R2.Text = "-"
+        Me.C1R2.UseVisualStyleBackColor = False
+        '
+        'C3R3
+        '
+        Me.C3R3.BackColor = System.Drawing.SystemColors.Desktop
+        Me.C3R3.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.C3R3.Location = New System.Drawing.Point(469, 421)
+        Me.C3R3.Name = "C3R3"
+        Me.C3R3.Size = New System.Drawing.Size(222, 198)
+        Me.C3R3.TabIndex = 8
+        Me.C3R3.Text = "-"
+        Me.C3R3.UseVisualStyleBackColor = False
+        '
+        'C2R3
+        '
+        Me.C2R3.BackColor = System.Drawing.SystemColors.Desktop
+        Me.C2R3.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.C2R3.Location = New System.Drawing.Point(241, 421)
+        Me.C2R3.Name = "C2R3"
+        Me.C2R3.Size = New System.Drawing.Size(222, 198)
+        Me.C2R3.TabIndex = 7
+        Me.C2R3.Text = "-"
+        Me.C2R3.UseVisualStyleBackColor = False
+        '
+        'C1R3
+        '
+        Me.C1R3.BackColor = System.Drawing.SystemColors.Desktop
+        Me.C1R3.ForeColor = System.Drawing.SystemColors.ActiveCaption
+        Me.C1R3.Location = New System.Drawing.Point(13, 421)
+        Me.C1R3.Name = "C1R3"
+        Me.C1R3.Size = New System.Drawing.Size(222, 198)
+        Me.C1R3.TabIndex = 6
+        Me.C1R3.Text = "-"
+        Me.C1R3.UseVisualStyleBackColor = False
+        '
+        'Form2
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.ClientSize = New System.Drawing.Size(704, 635)
+        Me.Controls.Add(Me.C3R3)
+        Me.Controls.Add(Me.C2R3)
+        Me.Controls.Add(Me.C1R3)
+        Me.Controls.Add(Me.C3R2)
+        Me.Controls.Add(Me.C2R2)
+        Me.Controls.Add(Me.C1R2)
+        Me.Controls.Add(Me.C3R1)
+        Me.Controls.Add(Me.C2R1)
+        Me.Controls.Add(Me.C1R1)
+        Me.Name = "Form2"
+        Me.Text = "Main Game"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents C1R1 As System.Windows.Forms.Button
+    Friend WithEvents C2R1 As System.Windows.Forms.Button
+    Friend WithEvents C3R1 As System.Windows.Forms.Button
+    Friend WithEvents C3R2 As System.Windows.Forms.Button
+    Friend WithEvents C2R2 As System.Windows.Forms.Button
+    Friend WithEvents C1R2 As System.Windows.Forms.Button
+    Friend WithEvents C3R3 As System.Windows.Forms.Button
+    Friend WithEvents C2R3 As System.Windows.Forms.Button
+    Friend WithEvents C1R3 As System.Windows.Forms.Button
+End Class
---
+++ TicTacToeBasic/Form2.vb
@@ -0,0 +1,210 @@
+﻿Public Class Form2
+
+    Public turn As String = "X"
+
+    Public game As Boolean = False
+
+    Private Sub Form2_MouseClick(sender As System.Object, e As System.Windows.Forms.MouseEventArgs) Handles MyBase.MouseClick
+        MsgBox("Click!")
+    End Sub
+
+    Private Sub C1R1_MouseClick(sender As System.Object, e As System.Windows.Forms.MouseEventArgs) Handles C1R1.MouseClick
+        Me.Text = turn
+        Me.Enabled = False
+        CheckWin()
+        SwitchTurn()
+    End Sub
+
+    Private Sub C2R1_Click(sender As System.Object, e As System.EventArgs) Handles C2R1.Click
+        Me.Text = turn
+        Me.Enabled = False
+        CheckWin()
+        SwitchTurn()
+    End Sub
+
+    Private Sub C3R1_Click(sender As System.Object, e As System.EventArgs) Handles C3R1.Click
+        Me.Text = turn
+        Me.Enabled = False
+        CheckWin()
+        SwitchTurn()
+    End Sub
+
+    Private Sub C1R2_Click(sender As System.Object, e As System.EventArgs) Handles C1R2.Click
+        Me.Text = turn
+        Me.Enabled = False
+        CheckWin()
+        SwitchTurn()
+    End Sub
+
+    Private Sub C2R2_Click(sender As System.Object, e As System.EventArgs) Handles C2R2.Click
+        Me.Text = turn
+        Me.Enabled = False
+        CheckWin()
+        SwitchTurn()
+    End Sub
+
+    Private Sub C3R2_Click(sender As System.Object, e As System.EventArgs) Handles C3R2.Click
+        Me.Text = turn
+        Me.Enabled = False
+        CheckWin()
+        SwitchTurn()
+    End Sub
+
+    Private Sub C1R3_Click(sender As System.Object, e As System.EventArgs) Handles C1R3.Click
+        Me.Text = turn
+        Me.Enabled = False
+        CheckWin()
+        SwitchTurn()
+    End Sub
+
+    Private Sub C2R3_Click(sender As System.Object, e As System.EventArgs) Handles C2R3.Click
+        Me.Text = turn
+        Me.Enabled = False
+        CheckWin()
+        SwitchTurn()
+    End Sub
+
+    Private Sub C3R3_Click(sender As System.Object, e As System.EventArgs) Handles C3R3.Click
+        Me.Text = turn
+        Me.Enabled = False
+        CheckWin()
+        SwitchTurn()
+    End Sub
+
+    Private Sub CheckWin()
+        If game = False Then
+            '' [x] [ ] [ ]
+            '' [x] [ ] [ ]
+            '' [x] [ ] [ ]
+
+            If C1R1.Enabled = False And C1R2.Enabled = False And C1R3.Enabled = False Then
+                MsgBox(turn + " wins!")
+                GameOver()
+            End If
+
+
+            '' [ ] [x] [ ]
+            '' [ ] [x] [ ]
+            '' [ ] [x] [ ]
+
+            If C2R1.Enabled = False And C2R2.Enabled = False And C2R3.Enabled = False Then
+                MsgBox(turn + " wins!")
+                GameOver()
+            End If
+
+            '' [ ] [ ] [x]
+            '' [ ] [ ] [x]
+            '' [ ] [ ] [x]
+
+            If C3R1.Enabled = False And C3R2.Enabled = False And C3R3.Enabled = False Then
+                MsgBox(turn + " wins!")
+                GameOver()
+            End If
+
+            '' [x] [x] [x]
+            '' [ ] [ ] [ ]
+            '' [ ] [ ] [ ]
+
+            If C1R1.Enabled = False And C2R1.Enabled = False And C3R1.Enabled = False Then
+                MsgBox(turn + " wins!")
+                GameOver()
+            End If
+
+            '' [ ] [ ] [ ]
+            '' [x] [x] [x]
+            '' [ ] [ ] [ ]
+
+            If C1R2.Enabled = False And C2R2.Enabled = False And C3R2.Enabled = False Then
+                MsgBox(turn + " wins!")
+                GameOver()
+            End If
+
+            '' [ ] [ ] [ ]
+            '' [ ] [ ] [ ]
+            '' [x] [x] [x]
+
+            If C1R3.Enabled = False And C2R3.Enabled = False And C3R3.Enabled = False Then
+                MsgBox(turn + " wins!")
+                GameOver()
+            End If
+
+            '' [x] [ ] [ ]
+            '' [ ] [x] [ ]
+            '' [ ] [ ] [x]
+
+            If C1R1.Enabled = False And C2R2.Enabled = False And C3R3.Enabled = False Then
+                MsgBox(turn + " wins!")
+                GameOver()
+            End If
+
+            '' [ ] [ ] [x]
+            '' [ ] [x] [ ]
+            '' [x] [ ] [ ]
+
+            If C1R3.Enabled = False And C2R2.Enabled = False And C3R1.Enabled = False Then
+                MsgBox(turn + " wins!")
+                GameOver()
+            End If
+
+        End If
+
+    End Sub
+
+    Private Sub SwitchTurn()
+
+        If turn = "X" Then
+
+            turn = "Y"
+
+        End If
+
+        If turn = "Y" Then
+
+            turn = "X"
+
+        End If
+
+    End Sub
+
+    Private Sub GameOver()
+
+        C1R1.Enabled = False
+        C1R2.Enabled = False
+        C1R3.Enabled = False
+        C2R1.Enabled = False
+        C2R2.Enabled = False
+        C2R3.Enabled = False
+        C3R1.Enabled = False
+        C3R2.Enabled = False
+        C3R3.Enabled = False
+
+        game = True
+
+    End Sub
+
+
+    Private Sub NewGame()
+
+        C1R1.Enabled = True
+        C1R2.Enabled = True
+        C1R3.Enabled = True
+        C2R1.Enabled = True
+        C2R2.Enabled = True
+        C2R3.Enabled = True
+        C3R1.Enabled = True
+        C3R2.Enabled = True
+        C3R3.Enabled = True
+
+        turn = "X"
+
+        game = False
+
+    End Sub
+
+
+    Private Sub Form2_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        turn = "X"
+        game = False
+    End Sub
+
+End Class
